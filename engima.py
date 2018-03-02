6086498e2389cdca1fc22318604c375cfb6adab5
Henric <henric.helbro@gmail.com>
2018-03-02 09:49:12 +0100

    enigma.py

---
+++ engima.py
@@ -0,0 +1,30 @@
+import math
+import cmath
+
+# TODO: Construct the beahviour of each physical component of the Enigma in functions below
+
+
+def inputkeyboard():
+    keypress = 'T'
+
+def plugboard():
+    pass
+
+def staticRotor():
+    pass
+
+def rotors():
+    rotor1 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    rotor2 = rotor1[::-1]
+    print(rotor2)
+
+def reflector():
+    pass
+
+def reverseJourney():
+    pass
+
+def lampboard():
+    pass
+
+rotors()
